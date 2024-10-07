342dd6cfb339e39e60adc1b7035dd06d4068c0fe
Juan Pablo Arias <juanpabloarias803@gmail.com>
2024-10-07 14:10:01 -0400

    code cleaning, final version

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,14 +2,9 @@ import { defineRouting } from 'next-intl/routing';
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-    // A list of all locales that are supported
-    locales: ['en', 'es'],
-
-    // Used when no locale matches
+    locales: ['en', 'es'] as string[],
     defaultLocale: 'en'
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } =
     createSharedPathnamesNavigation(routing);
